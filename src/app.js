535bef0917f058099ecea052680199aa96273ea9
paola9896 <paolasc2652@gmail.com>
2021-07-22 20:27:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Arrays with their values
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //Random numbers for each array and their values
+  let random1 = Math.floor(Math.random() * who.length);
+  let random2 = Math.floor(Math.random() * action.length);
+  let random3 = Math.floor(Math.random() * what.length);
+  let random4 = Math.floor(Math.random() * when.length);
+
+  //Concatenation of the random values into an excuse
+  document.querySelector("#excuse").innerHTML =
+    who[random1] +
+    " " +
+    action[random2] +
+    " " +
+    what[random3] +
+    " " +
+    when[random4];
 };
